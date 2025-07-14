444bcc7ef7720561fe98ca6173a87047f8efafc6
Bob Witmer <bwitmer@golden.net>
2025-07-14 16:11:13 -0400

    Initial Commit

---
+++ FootballAVP/FootballAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballAVPApp.swift
+//  FootballAVP
+//
+//  Created by Bob Witmer on 2025-07-14.
+//
+
+import SwiftUI
+
+@main
+struct FootballAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FootballAVPTests/FootballAVPTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballAVPTests.swift
+//  FootballAVPTests
+//
+//  Created by Bob Witmer on 2025-07-14.
+//
+
+import Testing
+@testable import FootballAVP
+
+struct FootballAVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
